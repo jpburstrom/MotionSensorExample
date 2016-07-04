c6cb3fdfccd664b61aef4e4e2cc775344b6d5336
Michele Mocciciola <mimocciola@yahoo.com>
2016-07-04 16:50:37 +0200

    Modified output adding acceleration

--- main.c
+++ main.c
@@ -8,14 +8,16 @@
 #define delay_ms(a) usleep(a*1000)
 
 int main() {
-	ms_open();
-	do{
-		ms_update();
-		printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\ttemperature = %2.1f\tcompass = %2.1f, %2.1f, %2.1f\n",
-		 ypr[YAW], ypr[PITCH],
-		 ypr[ROLL],temp,compass[0],compass[1],compass[2]);
-		delay_ms(5);
-	}while(1);
-
-	return 0;
+  ms_open();
+  do {
+    ms_update();
+    printf("yaw = %2.1f\tpitch = %2.1f\troll = %2.1f\ttemperature = %2.1f\tcompass = %2.1f, %2.1f, %2.1f\taccel = %2.4f, %2.4f, %2.4f\n",
+	   ypr[YAW], ypr[PITCH], ypr[ROLL],
+	   temp,
+	   compass[0],compass[1],compass[2],
+	   accel[0], accel[1], accel[2] );
+    delay_ms(5);
+  } while(1);
+  
+  return 0;
 }
